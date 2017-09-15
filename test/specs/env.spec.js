16098a673e590751340a61e86ac146c049fb6820
rodrigo4244 <rodrigo4244@users.noreply.github.com>
2017-09-15 14:07:32 -0300

    chore: initial commit

---
+++ test/specs/env.spec.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const avow = require('avow')
+const { forEach } = require('lodash/fp')
+
+const envVars = [
+  'LOGGER_LEVEL',
+  'NODE_ENV',
+  'NPM_TOKEN',
+]
+
+describe('envVars', () => {
+  forEach(name => {
+    it(name, () => {
+      avow.notEqual(process.env[name], null, `${name} is not set`)
+    })
+  })(envVars)
+})
