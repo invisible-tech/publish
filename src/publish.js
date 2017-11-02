47d0fc8fbca46fc915d55ff873d101d32fa94039
rodrigo4244 <8367325+rodrigo4244@users.noreply.github.com>
2017-11-02 16:01:55 -0200

    chore: check if .npmrc is present and if true, do not overwrite

--- src/publish.js
+++ src/publish.js
@@ -1,14 +1,28 @@
 'use strict'
 
+const assert = require('assert')
+const fileExists = require('file-exists')
 const fs = require('fs')
+const path = require('path')
 const spawn = require('cross-spawn')
 
 const { currentBranch } = require('./helpers/index')
 
-const run = ({ NPM_TOKEN, NPMRC_DIR = process.env.HOME }) => {
+const run = ({ NPM_TOKEN, NPMRC_DIR = process.env.HOME } = {}) => {
   if (currentBranch() !== 'master') return undefined
 
-  fs.writeFileSync(`${NPMRC_DIR}/.npmrc`, `//registry.npmjs.org/:_authToken=${NPM_TOKEN}`, 'utf8')
+  const filePath = path.resolve(NPMRC_DIR, '.npmrc')
+
+  // Check if .npmrc file exists on given path.
+  const exists = fileExists.sync(filePath)
+
+  if (exists) {
+    spawn.sync('npm', ['publish', '--access=public'], { stdio: 'inherit' })
+    return true
+  }
+
+  assert(NPM_TOKEN, 'publish: NPM_TOKEN is required')
+  fs.writeFileSync(filePath, `//registry.npmjs.org/:_authToken=${NPM_TOKEN}`, 'utf8')
 
   spawn.sync('npm', ['publish', '--access=public'], { stdio: 'inherit' })
   return true
