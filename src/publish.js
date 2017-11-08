039f530e0fa931474350da9e321900f2fce0c1e0
rodrigo4244 <8367325+rodrigo4244@users.noreply.github.com>
2017-11-08 08:49:12 -0200

    refact: publish to return an object

--- src/publish.js
+++ src/publish.js
@@ -6,26 +6,40 @@ const fs = require('fs')
 const path = require('path')
 const spawn = require('cross-spawn')
 
-const { currentBranch } = require('./helpers/index')
+const {
+  currentBranch,
+} = require('./helpers/index')
 
 const run = ({ NPM_TOKEN, NPMRC_DIR = process.env.HOME } = {}) => {
-  if (currentBranch() !== 'master') return undefined
+  if (currentBranch() !== 'master') {
+    return {
+      pass: true,
+      msg: 'Not on master branch, skipping.',
+    }
+  }
 
   const filePath = path.resolve(NPMRC_DIR, '.npmrc')
 
   // Check if .npmrc file exists on given path.
   const exists = fileExists.sync(filePath)
 
-  if (exists) {
-    spawn.sync('npm', ['publish', '--access=public'], { stdio: 'inherit' })
-    return true
+  if (! exists) {
+    assert(NPM_TOKEN, 'publish: NPM_TOKEN is required')
+    fs.writeFileSync(filePath, `//registry.npmjs.org/:_authToken=${NPM_TOKEN}`, 'utf8')
   }
 
-  assert(NPM_TOKEN, 'publish: NPM_TOKEN is required')
-  fs.writeFileSync(filePath, `//registry.npmjs.org/:_authToken=${NPM_TOKEN}`, 'utf8')
+  const { error } = spawn.sync('npm', ['publish', '--access=public'], { stdio: 'inherit' })
+  if (error) {
+    return {
+      pass: false,
+      msg: error.message,
+    }
+  }
 
-  spawn.sync('npm', ['publish', '--access=public'], { stdio: 'inherit' })
-  return true
+  return {
+    pass: true,
+    msg: 'new version published to npm.',
+  }
 }
 
 module.exports = run
